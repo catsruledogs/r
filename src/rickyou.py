63c93a00816f5e4de9ffb88f1181572a6c229837
catsruleurick <119331551+catsruleurick@users.noreply.github.com>
2022-11-28 09:34:10 -0600

    Added credit to rickyou.py

--- src/rickyou.py
+++ src/rickyou.py
@@ -1,2 +1,3 @@
+# Credit goes to the person who wrote this on reddit: https://www.reddit.com/r/Python/comments/rp0t97/i_create_a_python_scripts_that_can_rickroll/
 import webbrowser as wb
 wb.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
